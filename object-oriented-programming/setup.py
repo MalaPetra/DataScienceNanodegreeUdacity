fee2233ad61df8e004183321d107c473c031448f
MalaPetra <39831897+MalaPetra@users.noreply.github.com>
2020-10-04 10:45:47 +0100

    adding setup file

---
+++ object-oriented-programming/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='clothing-package',
+      version='0.1.0',
+      author='Petra Mala',
+      author_email='bouckovap@gmail.com',
+      description='Basic python package',
+      packages=['clothing-package'],
+      zip_safe=False)
